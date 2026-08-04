631f176edf9044b05344a0a3c7bb488a4180113c
Haroon-jutt-780 <haroon.jutt4422@gmail.com>
2026-08-04 17:22:11 +0500

    Handle server down Errors and some fixes

--- UAF Result Scraper/main.py
+++ UAF Result Scraper/main.py
@@ -1,5 +1,5 @@
 import re, os
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Error, Playwright, TimeoutError, sync_playwright
 
 
 def display_welcome_banner() -> None:
@@ -18,7 +18,7 @@ def clear_screen() -> None:
 
 def get_ag() -> str:
     while True:
-        user_input = input("Enter AG Number (****-ag-****): ").strip().lower()
+        user_input = input("Enter your AG Number (****-ag-****): ").strip().lower()
         if re.fullmatch(r'\d{4}-ag-\d{1,5}', user_input):
             return user_input
         print("Invalid Input! Format must be YYYY-ag-XXXX.")
@@ -26,50 +26,64 @@ def get_ag() -> str:
 
 
 def run(playwright: Playwright) -> None:
+
+    LMS_URL = "https://lms.uaf.edu.pk/login/index.php"
+
+    #Gets AG from User
     ag_num = get_ag()
+
+    #Opens chromium
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
 
-    #Open portal and search for query
-    page.goto("https://lms.uaf.edu.pk/login/index.php")
-    page.get_by_role("textbox", name="Registration No").fill(ag_num)
-    page.get_by_role("button", name="Result").click()
-    page.wait_for_selector("table.table.tab-content")
+    try:
+        #Open portal and search for query
+        page.goto(LMS_URL, timeout=10000)
+        page.get_by_role("textbox", name="Registration No").fill(ag_num)
+        page.get_by_role("button", name="Result").click()
+        page.wait_for_selector("table.table.tab-content", timeout=10000)
 
-    #Stores both table's data separately
-    student_info_table = page.locator("table").nth(0)
-    course_result_table = page.locator("table").nth(1)
+        #Stores both table's data separately
+        student_info_table = page.locator("table").nth(0)
+        course_result_table = page.locator("table").nth(1)
 
-    info_rows = [
-        row.locator("td, th").all_inner_texts()
-        for row in student_info_table.locator("tr").all()
-    ]
+        info_rows = [
+            row.locator("td, th").all_inner_texts()
+            for row in student_info_table.locator("tr").all()
+        ]
 
-    result_rows = [
-        row.locator("td, th").all_inner_texts()
-        for row in course_result_table.locator("tr").all()
-    ]
+        result_rows = [
+            row.locator("td, th").all_inner_texts()
+            for row in course_result_table.locator("tr").all()
+        ]
 
-    print("=" * 50)
-    print(f"{info_rows[0][0]} {info_rows[0][1]}")
-    print(f"{info_rows[1][0]}: {info_rows[1][1]}")
-    print("=" * 50)
+        #Prints Result in formatted block
+        print("=" * 50)
+        print(f"{info_rows[0][0]} {info_rows[0][1]}")
+        print(f"{info_rows[1][0]}: {info_rows[1][1]}")
+        print("=" * 50)
 
+        for index, row in enumerate(result_rows):
+            print(f"{row[0]:<10}{row[3]:^10}{row[10]:^10}{row[11]:^10}")
+            if index == 0:
+                print("-" * 50)
 
-    for index, row in enumerate(result_rows):
-        print(f"{row[0]:<10}{row[3]:^10}{row[10]:^10}{row[11]:^10}")
-        if index == 0:
-            print("-" * 50)
+        print("=" * 50)
 
-    print("=" * 50)
+    except TimeoutError:
+        #Catches Timeout Error if server is down
+        print("\n[!] Error: LMS server timed out. The server might be down or unreachable.")
 
-    # ---------------------
-    context.close()
-    browser.close()
+    except Error as e:
+        # Catches any other Playwright issues (e.g., No Wi-Fi, bad URL, browser crash)
+        print(f"Playwright error: {e}")
+        
+    finally:
+        context.close()
+        browser.close()
 
 if __name__ == "__main__":
-    clear_screen()
     display_welcome_banner() 
     with sync_playwright() as playwright:
         run(playwright)
